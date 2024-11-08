9acec9f6e980837115cada41b68c3ad55e536ff7
root-zero-o <rootzero17@gmail.com>
2024-11-08 17:12:31 +0900

    feat: 일기 페이지 UI 구현 #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen font-pretendard">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
-        프리텐다드가 적용
+    <div className=" flex h-screen  font-pretendard">
+      <main className="w-full h-full flex flex-col items-center p-10 pb-16 gap-4">
+        <div className="flex flex-col gap-1 w-full">
+          <h1 className="font-black text-4xl">그림일기</h1>
+          <h4>오늘 하루를 그림으로 기록해보세요!</h4>
+        </div>
+        <div className="w-full h-full flex flex-col gap-4 border-gray-300 border-[1px] rounded-md shadow-lg p-6">
+          <div className="w-full flex">
+            <input
+              className="w-full h-10 p-2 border-b-2 bg-transparent text-2xl focus:ring-0 focus:outline-none"
+              placeholder="제목"
+              spellCheck="false"
+            />
+          </div>
+          <div className="w-full h-full border-gray-300 border-2 rounded-md">
+            그림 들어가는 곳
+          </div>
+          <textarea
+            placeholder="내용을 입력하세요"
+            spellCheck="false"
+            className="w-full h-60 bg-transparent text-xl focus:ring-0 focus:outline-none"
+          />
+        </div>
+        <div className="flex gap-2">
+          <button className="bg-gray-700 text-[var(--main-white)] w-fit p-2 rounded-md">
+            저장하기
+          </button>
+          <button className="bg-rose-400 text-[var(--main-white)] w-fit p-2 rounded-md">
+            삭제하기
+          </button>
+        </div>
       </main>
     </div>
   );
